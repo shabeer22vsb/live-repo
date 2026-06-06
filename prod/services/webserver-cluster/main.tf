01420b392411b7a471505f2b7d281c11862eaed4
shabeer22vsb <shabeer22vsb@gmail.com>
2026-06-06 12:08:04 +0100

    modified module

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 module "webserver-cluster" {
-  source  = "../../../modules/services/webserver-cluster"
+  source  = "https://github.com/shabeer22vsb/modules//services/webserver-cluster?ref=v.0.0.1"
   cluster_name = "PRODUCTION"
   db_remote_state_bucket = "my-tf-practical-bucket-166373406634-eu-west-1-an"
   db_remote_state_key = "stage/data-stores/prod/mysql/terraform.tfstate"
